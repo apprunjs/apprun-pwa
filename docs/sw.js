8da5d6da78d36b81b773828619f21561700d8ec9
Yiyi Sun <yiyisun@gmail.com>
2020-02-26 13:08:22 -0500

    move sw-config.js into sw.js

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,25 @@
-importScripts("sw-config.js")
+//This is the service worker with the Advanced caching
+const CACHE = "my-apprun-site";
+const precacheFiles = [
+  "index.html",
+  "dist/index.js"
+  // "style.css"
+];
+
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "offline.html";
+
+const networkFirstPaths = [
+  /* Add an array of regex of paths that should go network first */
+  // Example: /\/api\/.*/
+  "index.html",
+  /\/dist\/.*/
+];
+
+const avoidCachingPaths = [
+  /* Add an array of regex of paths that shouldn't be cached */
+  // Example: /\/api\/.*/
+];
 
 function pathComparer(requestUrl, pathRegEx) {
   return requestUrl.match(new RegExp(pathRegEx));
